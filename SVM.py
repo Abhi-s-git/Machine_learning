c49a553933aec98ac8d4ce030d9aec3d98ece962
Abhi-s-git <abhi.s.09.2005@gmail.com>
2025-10-26 12:52:57 +0530

    Update SVM.py

--- SVM.py
+++ SVM.py
@@ -5,7 +5,6 @@ from sklearn.metrics import accuracy_score
 
 iris = load_iris()
 X, y = iris.data, iris.target
-
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 svm_clf = SVC()
 svm_clf.fit(X_train, y_train)
